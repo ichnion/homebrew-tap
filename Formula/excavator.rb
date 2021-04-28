8ddde08c86a070aadbc07fb6157acc5d202c9987
chansuke <chansuke@georepublic.de>
2021-04-28 12:10:11 +0900

    v0.2.0

--- Formula/excavator.rb
+++ Formula/excavator.rb
@@ -1,15 +1,15 @@
 class Excavator < Formula
     desc "Visualize your digital footprint."
     homepage "https://ichnion.github.io/website/"
-    version "v0.1.2"
+    version "v0.2.0"
     license "GPL-3.0"
 
     if OS.mac?
       url "https://github.com/ichnion/excavator/releases/download/#{version}/excavator-macos.tar.gz"
-      sha256 '5b47360304851ac981825e20177822db5e5d2172516e397dc8a2feeb036540f5'
+      sha256 'ced73dc5de9af9eccffa2b27485cb3a054f35ddd4471a0c2bf0e100f390a7a44'
     elsif OS.linux?
       url "https://github.com/ichnion/excavator/releases/download/#{version}/excavator-linux.tar.gz"
-      sha256 '3736c4ff9ae8d42bc4861c4d313f5de3c018816b1789bd79f21c89dddb90776d'
+      sha256 'efdeb14dbca7ca3eb8a53508de541a08120d180e4f3d1e6f9eb4ad94d123fb4f'
     end
 
     def install
